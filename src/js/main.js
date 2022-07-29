57bb782b5e52d94818d1df8a609f4a84f325764b
Andrey <biondohod@gmail.com>
2022-07-29 17:53:50 +0200

    Добавляет методы для работы с классами элементов

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,9 @@
 import './lib/lib';
 
-$('.active').show();
-console.log($('div'));
+$('.active').hide().show('flex');
+
+const elem = $('div');
+elem.addClass('1', '2', '3', '5');
+elem.removeClass('2', '5');
+elem.toggleClass('4', '6');
+
